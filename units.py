5315faa9025ba7504643b913db0047e265379e13
Federico G Lopez Armengol <fedelopezar@gmail.com>
2019-08-06 00:32:15 -0300

    Added conversion to natural units, and back.

--- units.py
+++ units.py
@@ -1,11 +1,14 @@
 """ 
 UNIT TRANSFORMATIONS
 Just a bunch of functions for quick unit transformations.
+Author: Fede LA
 2019
 """
 def cctk2cgs(cctk, unit, M=1):
 	"""Converts cctk quantity to cgs, for a given mass scale M
-	cctk units assume c = G = M_sun = 1
+	CCTK UNITS
+	Are geometrical units, ie c = G = 1, but the unit of length has been set to gravitational radius M
+	Furthermore, the unit of M is the mass of the Sun (or a multiple M)
 
 	Arguments:
 	cctk (float) : cctk quantity to transform
@@ -16,19 +19,20 @@ def cctk2cgs(cctk, unit, M=1):
     	cgs (float) : cgs quantity transformed
 
    	"""
-	cactusM = 5.028916268544129e-34 / M
-	cactusL = 6.772400341316594e-6 / M
-	cactusT = 2.0303145448833407e5 / M
+	g = 5.028916268544129e-34 / M
+	cm = 6.772400341316594e-6 / M
+	s = 2.0303145448833407e5 / M
 	if(unit == 'length' or unit == 'distance'):
-		cgs = cctk / cactusL
+		cgs = cctk / cm
 	elif(unit == 'time'):
-		cgs = cctk / cactusT
+		cgs = cctk / s
+		print(cgs)
 	elif(unit == 'mass'):
-		cgs = cctk / cactusM
+		cgs = cctk / g
 	elif(unit == 'density'):
-		cgs = cctk * (cactusL * cactusL * cactusL) / cactusM
+		cgs = cctk * (cm * cm * cm) / g
 	elif(unit == 'energy density'):
-		cgs = cctk * (cactusL * cactusT * cactusT) / cactusM
+		cgs = cctk * (cm * s * s) / g
 	else:
 		print("ERROR: Unknown unit.")
 		return
@@ -36,7 +40,9 @@ def cctk2cgs(cctk, unit, M=1):
 
 def cgs2cctk(cgs, unit, M=1):
 	"""Converts cgs quantity to cctk, for a given mass scale M
-	cctk units assume c = G = M_sun = 1
+	CCTK UNITS
+	Are geometrical units, ie c = G = 1, but the unit of length has been set to gravitational radius M
+	Furthermore, the unit of M is the mass of the Sun (or a multiple M)
 
 	Arguments:
     	cgs (float) : cgs quantity to transform
@@ -47,19 +53,19 @@ def cgs2cctk(cgs, unit, M=1):
     	cctk (float) : cctk quantity transformed
 
    	"""
-	cactusM = 5.028916268544129e-34 / M
-	cactusL = 6.772400341316594e-6 / M
-	cactusT = 2.0303145448833407e5 / M
+	g = 5.028916268544129e-34 / M
+	cm = 6.772400341316594e-6 / M
+	s = 2.0303145448833407e5 / M
 	if(unit == 'length' or unit == 'distance'):
-		cctk = cgs * cactusL
+		cctk = cgs * cm
 	elif(unit == 'time'):
-		cctk = cgs * cactusT
+		cctk = cgs * s
 	elif(unit == 'mass'):
-		cctk = cgs * cactusM
+		cctk = cgs * g
 	elif(unit == 'density'):
-		cctk = cgs * cactusM / (cactusL * cactusL * cactusL)
+		cctk = cgs * g / (cm * cm * cm)
 	elif(unit == 'energy density'):
-		cctk = cgs * cactusM / (cactusL * cactusT * cactusT)
+		cctk = cgs * g / (cm * cm * cm)
 	else:
 		print("ERROR: Unknown unit.")
 		return
@@ -70,3 +76,85 @@ def cm2pc(cm):
 
 def s2year(s):
 	return s / 3.154e+7
+
+def natural2cgs(natural,unit):
+	"""Converts natural units quantity to cgs
+	NATURAL UNITS FOR ASTROPHYSICIST: 
+	Most measured in energy since: c = h_bar = k_B = 1, and the unit of E is 1eV
+	The unit of charge is fixed by eps0 = 1/4pi, and given by q = sqrt(alpha)
+	
+	Arguments:
+    	natural (float) : natural quantity to transform
+	unit (string) : nature of the unit to transform
+
+    	Returns:
+    	cgs (float) : cgs quantity transformed
+
+   	"""
+	g = 1 / 1.78E-33
+	cm = 1 / 1.97E-5
+	s = 1 / 6.58E-16
+	K = 1 / 1.16E4
+	C = 1 / 1.88E-18
+
+	if(unit == 'length' or unit == 'distance'):
+		cgs = natural / cm
+	elif(unit == 'time'):
+		cgs = natural / s
+	elif(unit == 'mass'):
+		cgs = natural / g
+	elif(unit == 'temperature'):
+		cgs = natural / K
+	elif(unit == 'electric charge'):
+		cgs = natural / C # Actually we are returning Coulombs
+	elif(unit == 'density'):
+		cgs = natural * (cm * cm * cm) / g
+	elif(unit == 'energy density'):
+		cgs = cctk * (cm * s * s) / g
+	else:
+		print("ERROR: Unknown unit.")
+		return
+	return cgs
+
+	
+def cgs2natural(cgs,unit):
+	"""Converts natural units quantity to cgs
+	NATURAL UNITS FOR ASTROPHYSICIST: 
+	Most measured in energy since: c = h_bar = k_B = 1, and the unit of E is 1eV
+	The unit of charge is fixed by eps0 = 1/4pi, and given by q = sqrt(alpha)
+	
+	Arguments:
+    	cgs (float) : cgs quantity to transform
+	unit (string) : nature of the unit to transform
+
+    	Returns:
+    	natural (float) : natural quantity transformed
+
+   	"""
+	g = 1 / 1.78E-33
+	cm = 1 / 1.97E-5
+	s = 1 / 6.58E-16
+	K = 1 / 1.16E4
+	C = 1 / 1.88E-18
+
+	if(unit == 'length' or unit == 'distance'):
+		natural = cgs * cm
+	elif(unit == 'time'):
+		natural = cgs * s
+	elif(unit == 'mass'):
+		natural = cgs * g
+	elif(unit == 'temperature'):
+		natural = cgs * K
+	elif(unit == 'electric charge'):
+		natural = cgs * C # Actually we are returning Coulombs
+	elif(unit == 'density'):
+		natural = cgs * g / (cm * cm * cm)
+	elif(unit == 'energy'):
+		natural = cgs * g * cm * cm / (s * s) 
+	elif(unit == 'energy density'):
+		natural = cgs * g / (cm * s * s)
+	else:
+		print("ERROR: Unknown unit.")
+		return
+	return natural
+	
